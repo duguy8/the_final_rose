3d2b4e2202b95175097f164fd01a1044fc468663
Kris Litman <duguy8@gmail.com>
2021-02-21 14:32:35 -0500

    User story 3 complete. Fully feature tested, using partials and resources in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #bachelorettes
   resources :bachelorettes, only: [:show] do
     resources :contestants, only: [:index]
   end
 
+  #contestants
   resources :contestants, only: [:show]
+
+  #outings
+  resources :outings, only: [:show]
 end
--- spec/features/contestants/contestants_show_page_spec.rb
+++ spec/features/contestants/contestants_show_page_spec.rb
@@ -21,6 +21,21 @@ RSpec.describe "When I visit a bachelorette/contestants index" do
       age: 25,
       hometown: "Chicago"
     )
+    @ski_trip = @man1.outings.create!(
+      name: "Ski Trip",
+      location: "Aspen, CO",
+      date: "01/22/2021"
+    )
+    @hike = @man1.outings.create!(
+      name: "Hike",
+      location: "Flatirons, CO",
+      date: "01/25/2021"
+    )
+    @dinner = @man1.outings.create!(
+      name: "Snarfs",
+      location: "S Broadway, CO",
+      date: "01/27/2021"
+    )
   end
 
   describe "I see the contestants name, age, hometown" do
@@ -41,6 +56,25 @@ RSpec.describe "When I visit a bachelorette/contestants index" do
       end
 
       expect(current_path).to eq(contestant_path(@man1.id))
+    end
+  end
+
+  describe "I see the season number conestant was on with description" do
+    it "See list of names of outings contestant has been on, link to outing show page" do
+
+      visit contestant_path(@man1.id)
+
+      expect(page).to have_content(@bach1.season_number)
+      expect(page).to have_content("Oh no, this was the most dramatic season!")
+      expect(page).to have_link(@ski_trip.name)
+      expect(page).to have_link(@hike.name)
+      expect(page).to have_link(@dinner.name)
+
+      within("#outing-#{@dinner.id}") do
+        click_link(@dinner.name)
+      end
+
+      expect(current_path).to eq(outing_path(@dinner.id))
     end
   end
 end
